f916b25a7214ef00dbbb09b080e97eef576644d9
ddb0240 <135185056+ddb0240@users.noreply.github.com>
2024-04-26 21:42:56 -0500

    Update app_router.dart

--- lib/routing/app_router.dart
+++ lib/routing/app_router.dart
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-import 'package:flutter/material.dart';
-
-import '../screens/create_password/ui/create_password.dart';
-import '../screens/forget/ui/forget_screen.dart';
-import '../screens/home/ui/home_screen.dart';
-import '../screens/login/ui/login_screen.dart';
-import '../screens/signup/ui/sign_up_sceen.dart';
-import 'routes.dart';
-
-class AppRouter {
-  Route? generateRoute(RouteSettings settings) {
-    switch (settings.name) {
-      case Routes.forgetScreen:
-        return MaterialPageRoute(
-          builder: (_) => const ForgetScreen(),
-        );
-      case Routes.homeScreen:
-        return MaterialPageRoute(
-          builder: (_) => const HomeScreen(),
-        );
-      case Routes.createPassword:
-        final arguments = settings.arguments;
-        if (arguments is List) {
-          return MaterialPageRoute(
-            builder: (_) => CreatePassword(
-              googleUser: arguments[0],
-              credential: arguments[1],
-            ),
-          );
-        }
-        return null;
-      case Routes.signupScreen:
-        return MaterialPageRoute(
-          builder: (_) => const SignUpScreen(),
-        );
-      case Routes.loginScreen:
-        return MaterialPageRoute(
-          builder: (_) => const LoginScreen(),
-        );
-    }
-    return null;
-  }
-}
-=======
 import 'package:flutter/material.dart';
 
 import '../screens/create_password/ui/create_password.dart';
@@ -91,4 +46,3 @@ class AppRouter {
     return null;
   }
 }
->>>>>>> 1babb62eac41d3aed47a9929398040634317ab1e
